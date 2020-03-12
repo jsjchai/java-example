b5d92552f76faa6265c846516de7ea1dc093882a
jsjchai <jsjchai@163.com>
2020-03-12 09:52:15 +0800

    Consumer

--- function-example/src/main/java/com/github/jsjchai/ConsumerExample.java
+++ function-example/src/main/java/com/github/jsjchai/ConsumerExample.java
@@ -1,18 +1,30 @@
 package com.github.jsjchai;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.function.Consumer;
 
 /**
  * @author jsjchai.
  */
-
+@Getter
+@Setter
 public class ConsumerExample {
 
+    private String str;
 
-    public static void main(String[] args) {
+
+    public static void main( String[] args ) {
         // void返回
         Consumer<Integer> consumer = System.out::println;
 
         consumer.accept(1);
+
+        Consumer<ConsumerExample> exampleConsumer = System.out::println;
+        exampleConsumer.andThen(e -> e.setStr("11111"));
+
+        ConsumerExample example = new ConsumerExample();
+        exampleConsumer.accept(example);
     }
 }
