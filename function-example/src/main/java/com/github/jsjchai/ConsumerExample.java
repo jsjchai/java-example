369f1b9a3ece0b713121f87efc58626103ef0721
jsjchai <jsjchai@163.com>
2020-03-12 09:54:02 +0800

    Consumer

--- function-example/src/main/java/com/github/jsjchai/ConsumerExample.java
+++ function-example/src/main/java/com/github/jsjchai/ConsumerExample.java
@@ -2,6 +2,7 @@ package com.github.jsjchai;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.function.Consumer;
 
@@ -10,6 +11,7 @@ import java.util.function.Consumer;
  */
 @Getter
 @Setter
+@ToString
 public class ConsumerExample {
 
     private String str;
