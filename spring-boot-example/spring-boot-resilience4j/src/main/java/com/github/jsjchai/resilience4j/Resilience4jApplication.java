cf181bc6e585e134fea5732df28e608af4cd1055
jsjchai <jsjchai@163.com>
2020-03-13 10:57:44 +0800

    resilience4j init

---
+++ spring-boot-example/spring-boot-resilience4j/src/main/java/com/github/jsjchai/resilience4j/Resilience4jApplication.java
@@ -0,0 +1,15 @@
+package com.github.jsjchai.resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chaicj
+ */
+@SpringBootApplication
+public class Resilience4jApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(Resilience4jApplication.class, args);
+    }
+}
